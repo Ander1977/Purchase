e1e6f5ab933fbd0e64d5fc72f76973aa5042db04
Ander1977 <kopa.andrej@gmail.com>
2020-06-09 11:09:17 +0300

    Modified

--- src/main/java/ru/netology/manager/CartManager.java
+++ src/main/java/ru/netology/manager/CartManager.java
@@ -6,16 +6,12 @@ public class CartManager {
     private PurchaseItem[] items = new PurchaseItem[0];
 
     public void add(PurchaseItem item) {
-        // создаём новый массив размером на единицу больше
         int length = items.length + 1;
         PurchaseItem[] tmp = new PurchaseItem[length];
-        // itar + tab
-        // копируем поэлементно
-        // for (int i = 0; i < items.length; i++) {
-        //   tmp[i] = items[i];
-        // }
+        for (int i = 0; i < items.length; i++) {
+           tmp[i] = items[i];
+        }
         System.arraycopy(items, 0, tmp, 0, items.length);
-        // кладём последним наш элемент
         int lastIndex = tmp.length - 1;
         tmp[lastIndex] = item;
         items = tmp;
@@ -23,8 +19,6 @@ public class CartManager {
 
     public PurchaseItem[] getAll() {
         PurchaseItem[] result = new PurchaseItem[items.length];
-        // перебираем массив в прямом порядке
-        // но кладём в результаты в обратном
         for (int i = 0; i < result.length; i++) {
             int index = items.length - i - 1;
             result[i] = items[index];
@@ -32,7 +26,6 @@ public class CartManager {
         return result;
     }
 
-    // наивная реализация
     public void removeById(int id) {
         int length = items.length - 1;
         PurchaseItem[] tmp = new PurchaseItem[length];
@@ -43,7 +36,6 @@ public class CartManager {
                 index++;
             }
         }
-        // меняем наши элементы
         items = tmp;
     }
 }
