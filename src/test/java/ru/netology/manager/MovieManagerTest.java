008212a7a680a37a6cf5b38bef09a323e0341a64
Ander1977 <kopa.andrej@gmail.com>
2020-06-16 09:17:57 +0300

    Added lombok.config

--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -115,4 +115,34 @@ class MovieManagerTest {
         Movie[] expected = new Movie[]{eleven, then, nine, eight, seven};
         assertArrayEquals(expected, actual);
     }
+
+
+    @Test
+    void shouldGetFiveMovie() {
+        MovieManager movieManager = new MovieManager(5);
+        Movie one = new Movie(1, "Время первых", "История", "https://", "https://");
+        Movie two = new Movie(2, "О чём говорят мужчины", "Фильм", "https://", "https://");
+        Movie three = new Movie(3, "Операция Ы", "комедия", "https://", "https://");
+        Movie four = new Movie(4, "Падение Ангела", "боевик", "https://", "https://");
+        Movie five = new Movie(5, "У холмов есть глаза", "ужасы", "https://", "https://");
+        Movie six = new Movie(6, "Кун-Фу панда", "мультфильм", "https://", "https://");
+        Movie seven = new Movie(7, "Выкрутасы", "комедия", "https://", "https://");
+        Movie eight = new Movie(8, "Звездные войны", "фантастика", "https://", "https://");
+        Movie nine = new Movie(9, "Интерны", "сериал", "https://", "https://");
+        Movie then = new Movie(10, "Мадагаскар", "мультфильм", "https://", "http://");
+        movieManager.addMovie(one);
+        movieManager.addMovie(two);
+        movieManager.addMovie(three);
+        movieManager.addMovie(four);
+        movieManager.addMovie(five);
+        movieManager.addMovie(six);
+        movieManager.addMovie(seven);
+        movieManager.addMovie(eight);
+        movieManager.addMovie(nine);
+        movieManager.addMovie(then);
+        movieManager.getAll();
+        Movie[] actual = movieManager.getAll();
+        Movie[] expected = new Movie[]{then,nine, eight, seven, six};
+        assertArrayEquals(expected, actual);
+    }
 }
